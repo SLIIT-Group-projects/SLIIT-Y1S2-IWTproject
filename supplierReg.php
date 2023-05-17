e3aa173bf270d78059bee73d736d585c7c79490d
silunirupasinghe <silunirupasinghe@gmail.com>
2023-05-17 22:54:47 +0530

    Update supplierReg.php

--- supplierReg.php
+++ supplierReg.php
@@ -10,24 +10,24 @@
 
 <body>
 <!-- Including Header -->
-<?php include 'header.php'
-<h1>Supplier Registration</br></h1>
-<form method="POST" action="#">
-Company Registerd Name: 
+<?php include 'header.php';
+
+?>
+<h1><center>Supplier Registration</br></h1>
+<form method="POST" action="#" class="frm">
+Company Registerd Name: <br>
 <input type="text" placeholder="ABC (pvt) Ltd" name="cname">
-<br>Company Address: 
+<br>Company Address: <br>
 <textarea name="address" placeholder="6th lane, colombo 11"></textarea>
-<br>Contact Number: 
+<br>Contact Number: <br>
 <input type="number" placeholder="0711497951"  pattern="[0-9]{10}">
-<br>Email adress:
+<br>Email adress:<br>
 <input type="email" name="email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br>
 Company director details: <br>
-name:
+name:<br>
 <input type="text" placeholder="ABC (pvt) Ltd" name="Dname"><br>
 
-
-</form>
-?>
+<form>
 
 <hr>
 
