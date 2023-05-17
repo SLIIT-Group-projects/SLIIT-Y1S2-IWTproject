980d0cb2030fa17b34ec7b9b27ff67a91f35bb70
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-05-18 01:33:17 +0530

    about us page half complete

--- about.php
+++ about.php
@@ -1,21 +1,73 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>WeeNet</title>
+    <title>WeeNet</title>
+
     <!-- link css file -->
-	<link rel="stylesheet" href="css/style.css">
-    <!-- link js file -->
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="CSS/style.css">
 </head>
 
 <body>
-<!-- Including Header -->
-<?php include 'header.php';?>
+    <!-- Including Header -->
+    <?php include 'header.php'; ?>
 
-<hr>
+    <hr>
+    <!-- about us page section 1 -->
+    <section class="abt_section1">
+        <div class="container abt_sect1_container">
+            <div class="abt_sect1_box1">
+                <div class="">
+                    About Us
+                </div>
+            </div>
+        </div>
+    </section>
 
 
-<!--Including footer -->
-<?php include 'footer.php';?>
+    <!-- about us page section 2 -->
+    <section class="abt_section2">
+        <div class="container abt_sect2_container">
+            <div class="abt_sect2_box1">
+                <div class="abt_sect2_heading">Our story</div>
+            </div>
+            <div class="abt_sect2_box2">
+                <div class="abt_sect2_image"></div>
+                <div class="abt_sect2_para">We were made to known best name for our quality production of apparel manufacturing. The chase will not end creating best product for our valuable customers. We are normally assign to making several categories of sportswear, swimwear, and athleisure. Today we have made about 1 billion of products. As we continue to expand across borders.</div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- about us page section 3 -->
+
+    <section class="abt_section3">
+        <div class="container abt_sect3_container">
+            <div class="abt_sect3_box1">
+                <div class="abt_sect3_box2">
+                    <div class="abt_sect3_heading"><span class="abt_sect3_heading_span">Vision</span></div>
+                    <div class="abt_sect3_para">Our vision is to be a leader of apparel manufacturing industry in globally. Through the technology and innovate new apparel materials and products to the market.</div>
+                </div>
+                <div class="abt_sect3_img1"></div>
+            </div>
+
+            <div class="abt_sect3_box3">
+
+                <div class="abt_sect3_img2"></div>
+
+                <div class="abt_sect3_box4">
+                    <div class="abt_sect3_heading"><span class="abt_sect3_heading_span">Mission</span></div>
+                    <div class="abt_sect3_para">To deliver high quality apparel products to our customers with fast delivery. 100% satisfaction of customer need.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--Including footer -->
+    <?php include 'footer.php'; ?>
+
+    <!-- link js file -->
+    <script src="js/script.js"></script>
 </body>
+
 </html>
