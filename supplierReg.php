110778239ae6f33af1dab17597c91d2483e5d3f4
silunirupasinghe <silunirupasinghe@gmail.com>
2023-05-17 22:13:11 +0530

    Update supplierReg.php

--- supplierReg.php
+++ supplierReg.php
@@ -11,10 +11,16 @@
 <body>
 <!-- Including Header -->
 <?php include 'header.php';
-<h1>echo "Supplier Registration";
-
-
-
+echo "<h1>Supplier Registration</h1>";
+<form method="POST" action="#">
+echo "Company Registerd Name: ";
+<input type="text" placeholder="ABC (pvt) Ltd" name="name">
+echo "<br>Company Address: ";
+<textarea name="address" placeholder="6th lane, colombo 11"></textarea>
+echo "<br>Contact Number: ";
+<input type="number" placeholder="0711497951"  pattern="[0-9]{10}">
+echo "<br>Email adress:";
+<input type="email" name="email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
 
 ?>
 
