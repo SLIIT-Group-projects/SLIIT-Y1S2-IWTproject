3818bd50c5a978cc92f0d01f8b755b1b37905927
silunirupasinghe <silunirupasinghe@gmail.com>
2023-05-17 22:18:42 +0530

    Update supplierReg.php

--- supplierReg.php
+++ supplierReg.php
@@ -22,6 +22,8 @@ echo "<br>Contact Number: ";
 echo "<br>Email adress:";
 <input type="email" name="email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
 
+
+</form>
 ?>
 
 <hr>
