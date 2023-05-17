963f943e0d5f8f1056e94ce886f41ffcc464be2b
W S S De Silva <wsasanka.shenal@gmail.com>
2023-05-17 08:17:10 +0530

    footer

--- index.php
+++ index.php
@@ -1,1 +1,72 @@
-fhfhfhfhfh
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WeeNet</title>
+	<!-- website icon -->
+	<link rel = "icon" href = "resources/home/icon.jpg"
+	type = "image/x-icon">
+    <!-- link css file -->
+	<link rel="stylesheet" href="css/style.css">
+    <!-- link js file -->
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+
+<div class="header">
+  <img src="resources/home/logo.jpg" width="50px" height="50px" alt="WeeNet logo" class="logo">
+  <div class="search-bar">
+  <input type="text" class="search-input" placeholder="Search...">
+  <button class="search-button">Search</button>
+  </div>
+  <a href="index.php"><img src="resources/home/hedr/heart.png" width="30px" height="25px" alt="wishlist"></a>
+</div>
+
+    
+<center>
+<ul class="menu1">
+<li class="menu"><a href="index.php">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Home</a></li>
+<li class="menu"><a href="products.php">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Our Products</a></li>
+<li class="menu"><a href="career.php">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Vacancies</a></li>
+<li class="menu"><a href="contact.php">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Contact Us</a></li>
+<li class="menu"><a href="about.php">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;About Us</a></li>
+</ul>
+</center>
+
+
+<br>
+<hr>
+
+<footer>
+<center>
+<ul class="menu2">
+    <button><li class="menu"><a1 href="index.php" class="button">&emsp;&emsp;&nbsp;Help</a1></li></button>
+    <button><li class="menu"><a1 href="contact.php" class="button">&nbsp;&nbsp;&nbsp;Contact Us</a1></li></button>
+    <button><li class="menu"><a1 href="about.php" class="button">&nbsp;&emsp;About Us</a1></li></button>
+    <button><li class="menu"><a1 href="privacy.php" class="button">&nbsp;&nbsp;&emsp;Privacy</a></li></button>
+</center>
+</ul>
+<p> &emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp; &emsp;&emsp; &emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp; &emsp;Help Center&emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp;Live Chat&emsp;&emsp;  &emsp;&emsp;&emsp;&emsp;About WeeNet &nbsp;&emsp;&emsp; &emsp;&emsp; &nbsp;&emsp; Legal Imprint  
+<p> &emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp; &emsp;Security Center&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Email&emsp; &emsp;  &emsp; &emsp;&emsp; &emsp; &emsp; &nbsp;Blogs &nbsp; &emsp; &emsp; &emsp;&emsp;&emsp; &emsp;&emsp; &nbsp;&emsp; Policies
+
+<p> &emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp; &emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp; &emsp; &emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; Phone&emsp; &emsp; &emsp;&emsp;&emsp;&emsp; &emsp; &nbsp;Careers &nbsp; &emsp; &emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &nbsp;&emsp; Terms
+<p> &emsp;&emsp;&nbsp; &emsp;&emsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&emsp; &nbsp;&emsp; &nbsp;&emsp; &nbsp;&nbsp;&emsp; 
+
+<center>
+    <hr>
+    <p>©
+    <script>document.write(new Date().getFullYear());</script>
+
+    WeeNet(PVT)Ltd.&emsp; &emsp; &emsp;Terms of Use.&emsp; &emsp; &emsp;All Rights Reserved.&emsp; &emsp; &emsp;
+    <a href="https://twitter.com/SLIITinfo"><img src="resources/home/fticons/twitter.png" alt="twitter account" style="width:15px;height:15px;"></a>
+    &emsp; <a href="https://www.facebook.com/sliit.lk"><img src="resources/home/fticons/fb.png" alt="facebook account" style="width:15px;height:15px;"></a>
+    &emsp; <a href="https://www.instagram.com/sliit.life/"><img src="resources/home/fticons/insta.png" alt="instagram account" style="width:15px;height:15px;"></a>   
+    &emsp; <a href="https://www.tiktok.com/@sliit.life"><img src="resources/home/fticons/tiktok.png" alt="toktok account" style="width:15px;height:15px;"></a>
+    &emsp; <a href="https://www.youtube.com/user/SLIITtube"><img src="resources/home/fticons/youtube.png" alt="youtube account" style="width:15px;height:15px;"></a>
+    &emsp; <a href="https://www.linkedin.com/?trk=seo-authwall-base_nav-header-logo"><img src="resources/home/fticons/linkdin.png" alt="linkdin account" style="width:15px;height:15px;"></a>
+    </p> 
+</center>        
+</footer>
+
+</body>
+</html>
