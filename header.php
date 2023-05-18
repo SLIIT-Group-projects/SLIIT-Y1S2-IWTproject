0de835731ad723de04c4dd9707e5a2446551b0fa
W S S De Silva <wsasanka.shenal@gmail.com>
2023-05-18 13:25:32 +0530

    header new

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <header>
     <title>WeeNet</title>
     <!-- website icon -->
-    <link rel="icon" href="resources/home/icon.jpg" type="image/x-icon">
+	<link rel = "icon" href="resources/home/icon.jpg" type = "image/x-icon">
     <!-- link css file -->
     <link rel="stylesheet" href="css/style.css">
     <!-- link js file -->
@@ -12,15 +12,35 @@
 
 <body>
 
-
+    <!-- header line-->
 
     <div class="header">
+        <!-- weenet logo-->
         <img src="resources/home/logo.jpg" width="50px" height="50px" alt="WeeNet logo" class="logo">
-        <div class="search-bar">
-            <input type="text" class="search-input" placeholder="Search...">
-            <button class="search-button">Search</button>
+        <!-- search bar-->
+        <div class="search">
+            <div class="search-box">
+                <div class="search-field">
+                    <input placeholder="Search..." class="input" type="text">
+                        <div class="search-box-icon">
+                            <button class="btn-icon-content">
+                                <i class="search-icon">
+                                <svg xmlns="://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#fff"></path></svg>
+                                </i>
+                            </button>
+                        </div>
+                </div>
+            </div>
         </div>
+        <!-- wishlist heart -->
         <a href="index.php"><img src="resources/home/hedr/heart.png" width="30px" height="25px" alt="wishlist"></a>
+        <!-- cart logo-->
+        <a href="index.php"><img src="resources/home/fticons/cart.png" width="40px" height="43px" alt="wishlist"></a>
+        <!-- notification icon-->
+        <a href="index.php"><img src="resources/home/fticons/letter.png" width="40px" height="35px" alt="wishlist"></a>
+        <!-- profile logo-->
+        <a href="index.php"><img src="resources/home/fticons/dmenu.png" width="35px" height="35px" alt="wishlist"></a>
+        <!-- dropdown menu -->
     </div>
 
 
