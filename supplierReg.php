da2e11d4ff3f05bd4c7b9d667b10409df0d39184
silunirupasinghe <silunirupasinghe@gmail.com>
2023-05-18 21:18:43 +0530

    as at 5/18

--- supplierReg.php
+++ supplierReg.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WeeNet</title>
+	<title>Supplier registration</title>
     <!-- link css file -->
 	<link rel="stylesheet" href="css/style.css">
     <!-- link js file -->
     <script src="js/script.js"></script>
+    <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      padding: 20px;
+    }    
+    form {
+      max-width: 1000px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+
+    </style>
 </head>
 
 <body>
@@ -13,12 +29,14 @@
 <?php include 'header.php';
 
 ?>
+<h1>
 <h1><center>Supplier Registration</br></h1>
-<form method="POST" action="#" class="frm">
+<form method="POST" action="#">
+<h4 class="f1" >
 Company Registerd Name: <br>
 <input type="text" placeholder="ABC (pvt) Ltd" name="cname">
 <br>Company Address: <br>
-<textarea name="address" placeholder="6th lane, colombo 11"></textarea>
+<textarea name="address" placeholder="6th lane, colombo 11" cols rows=5></textarea>
 <br>Contact Number: <br>
 <input type="number" placeholder="0711497951"  pattern="[0-9]{10}">
 <br>Email adress:<br>
@@ -26,8 +44,9 @@ Company Registerd Name: <br>
 Company director details: <br>
 name:<br>
 <input type="text" placeholder="ABC (pvt) Ltd" name="Dname"><br>
-
-<form>
+</h4>
+</h1>
+</form>
 
 <hr>
 
