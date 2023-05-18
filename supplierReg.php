a04b1539d82362d904502bfb3952feec540a27ec
silunirupasinghe <silunirupasinghe@gmail.com>
2023-05-18 22:40:55 +0530

    2023-05-15 last

--- supplierReg.php
+++ supplierReg.php
@@ -19,8 +19,24 @@
       padding: 20px;
       border: 1px solid #ccc;
       border-radius: 5px;
+      background-color:#3e3e3e;
+      color:white;
     }
-
+    label{
+        font-color:white;
+    }
+    input{
+      background-color: white;
+      color:black;
+      padding: 10px 15px;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer; 
+    }
+    p1{
+        border: 2px solid white;
+        max-width: 750px;
+    }
     </style>
 </head>
 
@@ -29,24 +45,32 @@
 <?php include 'header.php';
 
 ?>
-<h1>
-<h1><center>Supplier Registration</br></h1>
-<form method="POST" action="#">
-<h4 class="f1" >
-Company Registerd Name: <br>
-<input type="text" placeholder="ABC (pvt) Ltd" name="cname">
-<br>Company Address: <br>
-<textarea name="address" placeholder="6th lane, colombo 11" cols rows=5></textarea>
-<br>Contact Number: <br>
-<input type="number" placeholder="0711497951"  pattern="[0-9]{10}">
-<br>Email adress:<br>
-<input type="email" name="email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br>
+
+<h1><center>Supplier Registration</center></br></h1>
+<form method="POST" action="#"><center>
+
+<label>Company Registerd Name:</lable></br> 
+<input type="text" placeholder="ABC (pvt) Ltd" name="cname"><br><br>
+<label>Company Address:  </lable></br> 
+<textarea name="address" placeholder="6th lane, colombo 11" cols rows=5></textarea><br><br>
+<label>Contact Number:</lable><br>
+<input type="number" placeholder="0711497951"  pattern="[0-9]{10}"><br><br>
+<label>Email adress:</lable><br>
+<input type="email" name="email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br><br>
 Company director details: <br>
-name:<br>
-<input type="text" placeholder="ABC (pvt) Ltd" name="Dname"><br>
-</h4>
-</h1>
-</form>
+<label>name:</lable><br>
+<input type="text" placeholder="Kamal Perera" name="Dname"><br><br>
+<label>NIC No:</lable><br>
+<input type="text" placeholder="200253402335" name="NIC"><br><br>
+<label>Contact Number:</lable><br>
+<input type="number" placeholder="071258258" name="DContactNo"><br><br>
+Material details:<br>
+Select the materials and the supplies available<br><br><br>
+     <p1>agreement details</p1><br>
+
+<input type="checkbox" > agree to the above mentioned coditions.<br>
+
+</form></center>
 
 <hr>
 
