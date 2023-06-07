313ef808eda00acb4012d7bdd7921e8b5ce25999
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-06-07 12:49:10 +0530

    profile page

---
+++ profile.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+      <!-- Including Header -->
+      <?php include 'header.php'; ?>
+
+
+    <div class="container">
+       <div class="pro_box1">
+            <div class="pro_box1_heading">Profile</div>
+            <div class="pro_box1_image"></div>
+       </div> 
+       <div class="pro_box2"></div> 
+    </div>
+
+     <!--Including footer -->
+     <?php include 'footer.php'; ?>
+</body>
+</html>
