e6f360cd90d167aff6b1a4d639722c5d85eaf4f0
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2023-06-07 17:25:02 +0530

    connection added

---
+++ connection/Dahamconfig.php
@@ -0,0 +1,9 @@
+<?php
+    $conn=new mysqli("localhost","root","Dahammysql@123","conncetion_checker");
+    if($conn->connect_error){
+        die("Connection error".$conn->connect_error);
+
+    }else{
+        echo "connection success";
+    }
+?>
--- profile.php
+++ profile.php
@@ -1,21 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
       <!-- Including Header -->
       <?php include 'header.php'; ?>
+      <?php include 'connection/Dahamconfig.php'; ?>
 
 
     <div class="container">
        <div class="pro_box1">
             <div class="pro_box1_heading">Profile</div>
-            <div class="pro_box1_image"></div>
+            <div class="pro_box1_image">
+                <?php
+                $sql="SELECT * FROM employee2;";
+                $result=$conn->query($sql);
+                if ($result->num_rows>0) {
+                    while($row = $result->fetch_assoc()){
+                        echo "id".$row["id"]." - Name: " . $row["first_name"]. " " . $row["last_name"]." - Address: " . $row["address"]." - age: " . $row["age"]. "<br>";
+                    }
+                }else{
+                    echo "no records";
+                }
+
+                $conn->close();
+                ?>
+            </div>
        </div> 
        <div class="pro_box2"></div> 
     </div>
