980f26fd2aebef588d90acd796a86c3599239ec8
silunirupasinghe <silunirupasinghe@gmail.com>
2023-06-08 19:04:57 +0530

    products

--- products.php
+++ products.php
@@ -6,6 +6,19 @@
 	<link rel="stylesheet" href="css/style.css">
     <!-- link js file -->
     <script src="js/script.js"></script>
+
+    <style>
+        container {
+                    padding: 0;
+                    display: flex;
+                    height: 100vh;
+                    flex-direction: column;
+                    align-items: center;
+                    padding-top: 80px;
+                    gap: 70px;
+                    }
+        
+    </style>
 </head>
 
 <body>
@@ -14,10 +27,23 @@
 
 <h2>Shop Here</h2>
 <h3>women</h3>
-<img src="resources/product1.jpeg">
-<img src="resources/product2.jpeg">
-<img src="resources/product3.jpeg">
-<p>Women's sleeveless running skinny</p>
+<div class="container">
+    <div>
+        <div><img src="resources/product1.jpeg"></div>
+        <p>Women's sleeveless running skinny</p>
+    </div>
+
+    <div>
+        <div><img src="resources/product2.jpeg"></div>
+        <p>Women's sleeveless running skinny</p>
+    </div>
+
+    <div>
+        <div><img src="resources/product3.jpeg"></div>
+        <p>Women's sleeveless running skinny</p>
+    </div>
+</div>
+</section>
 <hr>
 
 
