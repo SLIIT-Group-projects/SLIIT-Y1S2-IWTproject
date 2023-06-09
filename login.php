d2c6043108f6e805bc9e4bd63e0eac1f303e1d14
W S S De Silva <wsasanka.shenal@gmail.com>
2023-06-09 23:59:20 +0530

    make cookie

--- login.php
+++ login.php
@@ -1,112 +1,140 @@
 <?php
+session_start();
 
-session_start ();
+// Check if the user is already logged in and redirect to event.php
+// if (isset($_SESSION['user_id'])) {
+//     header('Location: event.php');
+//     exit;
+// }
 
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $connection = new mysqli('localhost', 'root', '', 'iwts2');
+
+    if ($connection->connect_error) {
+        die("Connection error: " . $connection->connect_error);
+    }
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1";
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param('ss', $email, $password);
+    $stmt->execute();
+    $result_set = $stmt->get_result();
+
+    if ($result_set && $result_set->num_rows == 1) {
+        $user = $result_set->fetch_assoc();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['email'] = $user['email'];
+
+        header('Location: index.php');
+        exit;
+    } else {
+        $error_message = "Invalid email or password";
+    }
+}
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WeeNet</title>
-	<!-- link css file -->
-	<link rel="stylesheet" href="css/style.css">
-	<!-- link js file -->
-	<script src="js/script.js"></script>
-    
-	<style>
-		.main {
-		  flex-direction: column;
-		  overflow: hidden;
-		  background-image: url("resources/login/l6.jpg");
-		  width: 1285px;
-		  height: 800px;
-		  background-size: cover;
-		  background-position: center;
-		  background-repeat: no-repeat;
-		  border-radius: 12px;
-		  box-shadow: 7px 7px 10px 3px #24004628;
-		  padding: 24px;
-		  color: whitesmoke;
-		  text-align: center;
+    <title>WeeNet</title>
+    <!-- link css file -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- link js file -->
+    <script src="js/script.js"></script>
 
-          
+    <style>
+        .main {
+          flex-direction: column;
+          overflow: hidden;
+          background-image: url("resources/login/l6.jpg");
+          width: 1285px;
+          height: 800px;
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+          border-radius: 12px;
+          box-shadow: 7px 7px 10px 3px #24004628;
+          padding: 24px;
+          color: whitesmoke;
+          text-align: center;
+
           /* Centering content */
           display: flex;
           justify-content: center;
           align-items: center;
-		}
+        }
 
-		.label {
-		  margin: 10px 5;
-		  font-size: 3rem;
-		  font-weight: bold;
-		  cursor: pointer;
-		  transition: .5s ease-in-out;
-		}
+        .label {
+          margin: 10px 5;
+          font-size: 3rem;
+          font-weight: bold;
+          cursor: pointer;
+          transition: .5s ease-in-out;
+        }
 
-		.input {
-		  width: 80%;
-		  height: 20px;
-		  background: #e0dede;
-		  border: none;
-		  outline: none;
-		  border-radius: 4px;
-		  margin-bottom: 1px;
-		}
+        .input {
+          width: 80%;
+          height: 20px;
+          background: #e0dede;
+          border: none;
+          outline: none;
+          border-radius: 4px;
+          margin-bottom: 1px;
+        }
 
-		.form button {
-		  width: 25%;
-		  height: 40px;
-		  margin-top: 2%;
-		  color: #fff;
-		  background: grey;
-		  font-size: 1rem;
-		  font-weight: bold;
-		  border: none;
-		  border-radius: 8px;
-		  cursor: pointer;
-		  transition: .2s ease-in;
-		  outline: none;
-		}
+        .form button {
+          width: 25%;
+          height: 40px;
+          margin-top: 2%;
+          color: #fff;
+          background: grey;
+          font-size: 1rem;
+          font-weight: bold;
+          border: none;
+          border-radius: 8px;
+          cursor: pointer;
+          transition: .2s ease-in;
+          outline: none;
+        }
 
-		.form button:hover {
-		  background-color: #6d44b8;
-		}
+        .form button:hover {
+          background-color: #6d44b8;
+        }
 
-		.register-link {
-		  margin-top: 10px;
-		  font-size: 0.9rem;
-		  color: #e0dede;
-		}
+        .register-link {
+          margin-top: 10px;
+          font-size: 0.9rem;
+          color: #e0dede;
+        }
 
-		.register-link a {
-		  color: #fff;
-		  font-weight: bold;
-		  text-decoration: none;
-		}
-	</style>
-
-	
+        .register-link a {
+          color: #fff;
+          font-weight: bold;
+          text-decoration: none;
+        }
+    </style>
 </head>
 <body>
+    <!-- Including Header -->
+    <?php include 'header.php'; ?>
 
-	<!-- Including Header -->
-	<?php include 'header.php'; ?>
+    <div class="main">
+        <h2 class="label">Log in</h2>
+        <form class="form" method="POST">
+            <input class="input" type="email" name="email" placeholder="Email" required="">
+            <input class="input" type="password" name="password" placeholder="Password" required="">
+            <p class="register-link">Don't have an account? <a href="register.php">Register</a></p>
+            <button type="submit" class="form-button" name="submit">Log in</button>
+        </form>
+    </div>
 
-	<div class="main">
-		<h2 class="label">Log in</h2>
-		<form class="form">
-			<input class="input" type="email" name="email" placeholder="Email" required="">
-			<input class="input" type="password" name="pswd" placeholder="Password" required="">
-			<p class="register-link">Don't have an account? <a href="register.php">Register</a></p>
-			<button class="button">Log in</button>
-		</form>
-	</div>
+    <hr>
 
-	<hr>
-
-	<!-- Including footer -->
-	<?php include 'footer.php'; ?>
+    <!-- Including footer -->
+    <?php include 'footer.php'; ?>
 </body>
 </html>
