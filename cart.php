c363a350677cc71447bdd101b9ccd37d79cd2e41
silunirupasinghe <silunirupasinghe@gmail.com>
2023-06-10 17:53:35 +0530

    siluni new pages added
    new one

---
+++ cart.php
@@ -0,0 +1,122 @@
+cart.php
+<?php 
+include_once 'config.php';
+?>
+
+<?php
+    session_start();
+    $db_name = "supplierreg";
+    $connection = new mysqli($servername, $username, $password, $db_name);
+
+    if (!isset($_SESSION["shopping_cart"])) {
+        $_SESSION["shopping_cart"] = array();
+    }
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>WeeNet</title>
+	<!-- website icon -->
+    <!-- link css file -->
+	<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>our products</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <style>
+        .product{
+            border: 1px solid #eaeaec;
+            margin: 2px 2px 8px 2px;
+            padding: 10px;
+            text-align: center;
+            background-color: #efefef;
+        }
+        table,th,tr{
+              text-align: center;
+        }
+        .title2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+        h2{
+            text-align: center;
+            color: #66afe9;
+            background-color: #efefef;
+            padding: 2%;
+        }
+        table th{
+            background-color: #efefef;
+        }
+    </style>
+</head>
+<body>
+    <!-- Including Header -->
+<?php include 'header.php'; ?>
+<?php
+    if(isset($_GET["action"])){
+        if($_GET["action"] == "delete"){
+            foreach($_SESSION["shopping_cart"] as $keys => $value){
+                if($value["product_id"] == $_GET["id"]){
+                    unset($_SESSION["shopping_cart"][$keys]);
+                    echo '<script>alert("Product has been removed")</script>';
+                    echo '<script>window.location="cart.php"</script>';
+                }
+            }
+        }
+    }
+?>
+       <div style="clear: both"></div>
+        <h3 class="title2">Shopping Cart Details</h3>
+        <div class="table-responsive">
+            <table class="table table-bordered">
+            <tr>
+                <th width="30%">Product Description</th>
+                <th width="10%">Quantity</th>
+                <th width="13%">Price Details</th>
+                <th width="10%">Total Price</th>
+                <th width="17%">Remove Item</th>
+            </tr>
+            <?php
+                if(!empty($_SESSION["shopping_cart"])){
+                    $total=0;
+                    foreach($_SESSION["shopping_cart"] as $key => $value){
+                    ?>
+                <tr>
+                        <td><?php echo $value["product_name"];?></td>
+                        <td><?php echo $value["product_quantity"];?></td>
+                        <td><?php echo $value["product_price"];?></td>
+                        <td><?php echo number_format($value["product_quantity"]*$value["product_price"],2);?></td>
+                        <td><a href="cart.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span class="text-danger">Remove Item</span></a></td>
+                </tr>
+                <?php
+                    $total = $total + ($value["product_quantity"]*$value["product_price"]);
+                    }
+                ?>
+                <tr>
+                        <td colspan="3" align="right">Total</td>
+                        <td align="right"><?php echo number_format($total,2);?></td>
+                        <td></td>
+                </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+
+    </div>
+    <div class="subbutton">
+    <input type="submit" value="checkout" style="background-color: white; color: black;" onclick="showAlert()">
+	</div>
+
+	<script>
+	function showAlert() {
+		alert("sucessful!");
+        header("Location:index.php");
+	}
+	</script>
+</body>
+</html>
---
+++ supplierReg.php
@@ -0,0 +1,147 @@
+<?php 
+include_once 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Supplier registration</title>
+    <!-- link css file -->
+  <link rel="stylesheet" href="css/style.css">
+    <!-- link js file -->
+    <script src="js/script.js"></script>
+    <style>
+    body {
+      font-size: 20px;
+      padding: 20px;
+    }    
+    form {
+      max-width: 1000px;
+      margin: 0 auto;
+      padding: 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      color:white;
+    }
+    label{
+        color:white;
+        font:'Poppins';
+    }
+  
+    input{
+      background-color: white;
+      color:black;
+      padding: 10px 15px;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer; 
+    }
+    p1{
+        border: 2px solid white;
+        max-width: 750px;
+    }
+
+    .container {
+        position: relative;
+        max-width: 1000px; /* Maximum width */
+      }
+
+    .container .content {
+        position: absolute; /* Position the background text */
+        bottom: 0; /* At the bottom. Use top:0 to append it to the top */
+        background: rgb(0, 0, 0); /* Fallback color */
+        background: rgba(0, 0, 0, 0.5); /* Black background with 0.5 opacity */
+        color: #f1f1f1; /* Grey text */
+        width: 173%; /* Full width */
+        padding: 20px; /* Some padding */
+       
+      }
+
+    #image_supplierReg{
+       height: 1400px;
+       width:1800px;
+      /* align:left; */
+      size: cover;
+      }
+    
+      input[type=text],textarea,select, input[type=email]{
+      width: 75%;
+      padding: 15px;
+      margin: 5px 0 22px 0;
+      display: inline-block;
+      border: none;
+      background: #f1f1f1;
+    }
+    .submitbtn {
+      background-color: #04AA6D;
+      color: white;
+      padding: 16px 20px;
+      margin: 8px 0;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+      opacity: 0.9;
+      font-size:20px;
+    }
+    .submitbtn:hover {
+      opacity:1;
+    }
+
+    label{
+      font-size:20px;
+      font: 'sans-serif';
+    }
+    </style>
+</head>
+
+<body>
+<!-- Including Header -->
+<?php include 'header.php';
+
+?>
+<div class="container">
+<img src="resources/supplierReg.jpg" id="image_supplierReg">
+
+<div class="content">
+  <div class="container2">
+<h1><center>Supplier Registration</center></br></h1>
+<form method="POST" action="submitSupplierReg.php"  ><center>
+<label>Company Registerd Name:</lable></br> 
+<input type="text" placeholder="ABC (pvt) Ltd" name="Registered_name"><br>
+<label>Company registration Number</label></br> 
+<input type="text" placeholder="123456" name="Registration_number"></br>
+<label>Company Address:  </lable></br> 
+<textarea name="Address" placeholder="6th lane, colombo 11" cols rows=5></textarea><br>
+<label>Contact Number:</lable><br>
+<input type="text" placeholder="0711497951" name="Contact_number" pattern="[0-9]{10}"><br>
+<label>Email adress:</lable><br>
+<input type="email" name="Email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br>
+<h3>Company director details: </h3>
+<label>name:</lable><br>
+<input type="text" placeholder="Kamal Perera" name="Director_name"><br>
+<label>NIC No:</lable><br>
+<input type="text" placeholder="200253402335" name="NIC"><br>
+<label>Contact Number:</lable><br>
+<input type="text" placeholder="071258258" name="Director_C_number"><br>
+<h3>Material details:<br>
+Select a materials available and type below</h3>
+<select name="material">
+  <option value="mat1"> Cotton </option>
+  <option value="mat2"> Silk </option>
+  <option value="mat3"> Wool </option></select></br></br>
+</selection>
+     agreement details</br>
+
+<input type="checkbox" class="inputStyle" id="checkbox">Accept privacy policy and terms</br><br>  
+<input type="submit" value="submit" class="submitbtn">
+
+</form></center>
+    </div>
+    </div>
+    </div>
+<hr>
+
+<!--Including footer -->
+<?php include 'footer.php';?>
+</body>
+</html>
