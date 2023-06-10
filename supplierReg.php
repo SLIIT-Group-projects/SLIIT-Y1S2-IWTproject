944346b5f75454565e7fe591a806d4aad24fb62c
silunirupasinghe <silunirupasinghe@gmail.com>
2023-06-10 09:18:50 +0530

    css

--- supplierReg.php
+++ supplierReg.php
@@ -12,7 +12,7 @@ include_once 'config.php';
     <script src="js/script.js"></script>
     <style>
     body {
-      font-family:  'Poppins',Arial, sans-serif;
+      fontsize 20px;
    
       padding: 20px;
     }    
@@ -59,12 +59,40 @@ include_once 'config.php';
       }
 
     #image_supplierReg{
-       height: 1100px;
+       height: 1400px;
        width:1800px;
       align:left;
       size: cover;
       }
-      
+      .container2{
+
+      }
+      input[type=text],textarea,select{
+      width: 100%;
+      padding: 15px;
+      margin: 5px 0 22px 0;
+      display: inline-block;
+      border: none;
+      background: #f1f1f1;
+    }
+    .submitbtn {
+      background-color: #04AA6D;
+      color: white;
+      padding: 16px 20px;
+      margin: 8px 0;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+      opacity: 0.9;
+    }
+    .submitbtn:hover {
+      opacity:1;
+    }
+
+    label{
+      font-size:20px;
+      font: 'sans-serif':
+    }
     </style>
 </head>
 
@@ -77,25 +105,26 @@ include_once 'config.php';
 <img src="resources/supplierReg.jpg" id="image_supplierReg">
 
 <div class="content">
+  <div class="container2">
 <h1><center>Supplier Registration</center></br></h1>
 <form method="POST" action="submitSupplierReg.php"  ><center>
 <label>Company Registerd Name:</lable></br> 
-<input type="text" placeholder="ABC (pvt) Ltd" name="Registered_name"><br><br>
+<input type="text" placeholder="ABC (pvt) Ltd" name="Registered_name"><br>
 <label>Company registration Number</label></br> 
-<input type="text" placeholder="123456" name="Registration_number"></br><br>
+<input type="text" placeholder="123456" name="Registration_number"></br>
 <label>Company Address:  </lable></br> 
-<textarea name="Address" placeholder="6th lane, colombo 11" cols rows=5></textarea><br><br>
+<textarea name="Address" placeholder="6th lane, colombo 11" cols rows=5></textarea><br>
 <label>Contact Number:</lable><br>
-<input type="text" placeholder="0711497951" name="Contact_number" pattern="[0-9]{10}"><br><br>
+<input type="text" placeholder="0711497951" name="Contact_number" pattern="[0-9]{10}"><br>
 <label>Email adress:</lable><br>
-<input type="email" name="Email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br><br>
+<input type="email" name="Email" placeholder="ABC@gmail.com"  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"><br>
 <h3>Company director details: </h3>
 <label>name:</lable><br>
-<input type="text" placeholder="Kamal Perera" name="Director_name"><br><br>
+<input type="text" placeholder="Kamal Perera" name="Director_name"><br>
 <label>NIC No:</lable><br>
-<input type="text" placeholder="200253402335" name="NIC"><br><br>
+<input type="text" placeholder="200253402335" name="NIC"><br>
 <label>Contact Number:</lable><br>
-<input type="text" placeholder="071258258" name="Director_C_number"><br><br>
+<input type="text" placeholder="071258258" name="Director_C_number"><br>
 <h3>Material details:<br>
 Select the materials and the supplies available</h3>
 <select name="material">
@@ -105,13 +134,13 @@ Select the materials and the supplies available</h3>
      agreement details</br>
 
 <input type="checkbox" class="inputStyle" id="checkbox">Accept privacy policy and terms</br><br>	
-<input type="submit" value="submit" id="submitBtn">
+<input type="submit" value="submit" class="submitbtn">
 
 </form></center>
     </div>
     </div>
+    </div>
 <hr>
-
 
 <!--Including footer -->
 <?php include 'footer.php';?>
