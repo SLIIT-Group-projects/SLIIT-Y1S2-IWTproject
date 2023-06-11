480328398471e6f7b7d75ad4c6cd1d58d727806c
W S S De Silva <wsasanka.shenal@gmail.com>
2023-06-11 12:30:35 +0530

    new sup

--- supDS.php
+++ supDS.php
@@ -1,43 +1,48 @@
 <?php
-// Create a connection
 $mysqli = new mysqli("localhost", "root", "", "iwtdb");
 
-// Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
-$mat = $_GET["material"];
 
-// Select query
-$query = "SELECT * FROM supplier WHERE mat='{$mat}'";
+$mat = isset($_GET["material"]) ? $_GET["material"] : "";
 
+$query = "SELECT * FROM supplierreg WHERE material = '{$mat}'";
 
-// Execute query
 $result = $mysqli->query($query);
 
-// Check if the query executed successfully
 if ($result->num_rows > 0) {
-    // Fetch the data as an associative array
-	echo'<table class="tablesup">';
-	//echo '<tr><th>Registration Number</th><th>Registered Name</th><th>Contact Number</th></tr>';
+    echo "<table border='1'>
+            <tr>
+                <th>Registration Number</th>
+                <th>Registered Name</th>
+                <th>Address</th>
+                <th>Contact Number</th>
+                <th>Email</th>
+                <th>Director Name</th>
+                <th>NIC</th>
+                <th>Director Contact Number</th>
+                <th>Material</th>
+            </tr>";
+
     while ($row = $result->fetch_assoc()) {
-        // Access the data using column names
-        echo '<tr>';
-        echo '<td>' . $row['Registration_number'] . '</td>';
-        echo '<td>' . $row['Registered_name'] . '</td>';
-        echo '<td>' . $row['contact_number'] . '</td>';
-        echo '</tr>';
-
-
-
-    // Iterate over each row and display the data
-   // foreach ($rows as $row)
-
-
-
-
+        echo "<tr>";
+        echo "<td>" . $row['Registration_number'] . "</td>";
+        echo "<td>" . $row['Registered_name'] . "</td>";
+        echo "<td>" . $row['Address'] . "</td>";
+        echo "<td>" . $row['Contact_number'] . "</td>";
+        echo "<td>" . $row['Email'] . "</td>";
+        echo "<td>" . $row['Director_name'] . "</td>";
+        echo "<td>" . $row['NIC'] . "</td>";
+        echo "<td>" . $row['Director_C_number'] . "</td>";
+        echo "<td>" . $row['material'] . "</td>";
+        echo "</tr>";
     }
-	echo'</table>';
+
+    echo "</table>";
 } else {
-    echo "Error: " . $mysqli->error;
+    echo "No suppliers found for the selected material.";
 }
+
+$mysqli->close();
+?>
--- supplier.php
+++ supplier.php
@@ -1,330 +1,258 @@
-<?php
-
-session_start();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>WeeNet</title>
-    <!-- link css file -->
-	<link rel="stylesheet" href="css/style.css">
-    <!-- link js file -->
+    <title>WeeNet</title>
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
-    
-<style>
-/* supplier.css */
+    <style>
+        /* supplier.css */
 
-.body1 {
-  font-family: Arial, sans-serif;
-  background-color: #f2f2f2;
-}
+        .body1 {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
 
-h1 {
-  text-align: center;
-}
+        h1 {
+            text-align: center;
+        }
 
-form {
-  max-width: 500px;
-  margin: 0 auto;
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-}
+        form {
+            max-width: 500px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
 
-label {
-  display: block;
-  margin-bottom: 10px;
-}
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
 
-input[type="submit"] {
-  background-color: #4caf50;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  cursor: pointer;
-  border-radius: 5px;
-}
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-  margin-bottom: 20px;
-}
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
 
-table th, table td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-}
+        table th,
+        table td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
 
-table th {
-  background-color: #f2f2f2;
-}
+        table th {
+            background-color: #f2f2f2;
+        }
 
-table td {
-  background-color: #fff;
-}
+        table td {
+            background-color: #fff;
+        }
 
-select, input[type="number"], input[type="date"], input[type="checkbox"] {
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
+        select,
+        input[type="number"],
+        input[type="date"],
+        input[type="checkbox"] {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
 
-input[type="checkbox"] {
-  width: auto;
-  margin-top: 10px;
-}
+        input[type="checkbox"] {
+            width: auto;
+            margin-top: 10px;
+        }
 
-input[type="submit"]:hover {
-  background-color: #45a049;
-}
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
 
-/* Center align the form */
-    form {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      text-align: center;
-    }
+        /* Center align the form */
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
 
-    /* Style the labels */
-    label {
-      font-weight: bold;
-    }
+        /* Style the labels */
+        label {
+            font-weight: bold;
+        }
 
-    /* Style the select dropdown */
-    select {
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 16px;
-      width: 200px;
-      margin-bottom: 10px;
-    }
+        /* Style the select dropdown */
+        select {
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            width: 200px;
+            margin-bottom: 10px;
+        }
 
-    /* Style the input fields */
-    input[type="number"],
-    input[type="date"] {
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 16px;
-      width: 200px;
-      margin-bottom: 10px;
-    }
+        /* Style the input fields */
+        input[type="number"],
+        input[type="date"] {
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            width: 200px;
+            margin-bottom: 10px;
+        }
 
-    /* Style the submit button */
-    input[type="submit"] {
-      padding: 10px 20px;
-      background-color: #4CAF50;
-      color: white;
-      border: none;
-      border-radius: 4px;
-      font-size: 16px;
-      cursor: pointer;
-    }
+        /* Style the submit button */
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
 
-    /* Center align the submit button */
-    #subbutton {
-      margin: 0 auto;
-    }
+        /* Center align the submit button */
+        #subbutton {
+            margin: 0 auto;
+        }
 
-	h1{
-	text-align: center;
-	}
-/* Center align the form */
-    form {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      text-align: center;
-    }
+        h1 {
+            text-align: center;
+        }
 
-    /* Style the labels */
-    label {
-      font-weight: bold;
-    }
-
-    /* Style the select dropdown */
-    select {
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 16px;
-      width: 200px;
-      margin-bottom: 10px;
-    }
-
-    /* Style the input fields */
-    input[type="number"],
-    input[type="date"] {
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      font-size: 16px;
-      width: 200px;
-      margin-bottom: 10px;
-    }
-
-    /* Style the submit button */
-    input[type="submit"] {
-      padding: 10px 20px;
-      background-color: #4CAF50;
-      color: white;
-      border: none;
-      border-radius: 4px;
-      font-size: 16px;
-      cursor: pointer;
-    }
-
-    /* Center align the submit button */
-    .subbutton {
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	  //height: 100vh; /* Adjust the height as per your requirement */
-	}
-
-
-
-	h1{
-	text-align: center;
-	}
-
-		.tablesup {
-			width: 40%;
-			height: 40px;s
-			text-align: center;
-			margin-left: auto;
-      margin-right: auto;
-
-		}
-
-
-</style>
-
-
-  <title>Supplier selection</title>
-
-
-  <!--script src="supplier.js"></script-->
-  <link rel="stylesheet" type="text/css" href="supplier.css">
-
-  <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   //Get form data
-  $supplier = $_POST["supplier"];
-  $material = $_POST["material"];
-  $quantity = $_POST["quantity"];
-  $supplierregNumber = $_POST["supplierregNumber"];
-  $companyID = $_POST["companyID"];
-  $deliveryDate = $_POST["delivery-date"];
-  $proceedPayment = isset($_POST["payment-proceed"]) ? "Yes" : "No"; // Checkbox value
-
-  // Process the data (You can modify this part to suit your needs)
-  // For example, you can save the data to a database or send it via email
-
-  // Print a confirmation message
-  echo "Thank you for your order!<br>";
-  echo "Supplier: " . $supplier . "<br>";
-  echo "Supplier registeration number:" .$supplierregNumber."<br>";
-  echo "Material: " . $material . "<br>";
-  echo "Quantity: " . $quantity . "<br>";
-  echo "Delivery Date: " . $deliveryDate . "<br>";
-  echo "Compnany ID:".$companyID. "<br>";
-  echo "Proceed with Payment: " . $proceedPayment . "<br>";
-}
-?>
-
-
-
-
-
+        .tablesup {
+            width: 40%;
+            height: 40px;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 
 <body>
-  <?php include 'header.php';?>
-<div class="body1" style = "font-family: 'Comic Sans', cursive;">
+    <?php include 'header.php'; ?>
+    <div class="body1">
+        <div style="background-image: url('css/supsel.jpeg'); background-repeat: no-repeat; background-size: cover">
+            <form class="form" style="background-color: white;">
+                <h1>Supplier selection</h1>
+            </form><br></br>
 
-<div style="background-image: url('css/supsel.jpeg'); background-repeat: no-repeat; background-size: cover">
+            <form class="form" style="background-color: white;" method="GET" action="supplier.php">
+                <label for="material">Choose material:</label>
+                <select id="material" name="material">
+                    <option value="cotton">cotton</option>
+                    <option value="silk">silk</option>
+                    <option value="wool">wool</option>
+                </select>
+                <input type="submit" value="OK" style="background-color: grey; color:white;">
+            </form><br></br>
 
-  <form class="form" style="background-color: white;">
-  <h1>Supplier selection </h1>
-  </form><br></br>
+            <?php
+            if (isset($_GET["material"])) {
+                $mat = $_GET["material"];
+                $mysqli = new mysqli("localhost", "root", "", "iwtdb");
 
-  <form class="form" style="background-color: white;">
+                if ($mysqli->connect_error) {
+                    die("Connection failed: " . $mysqli->connect_error);
+                }
 
-    <form method="GET" action ="supDS.php">
-    <label for="material">Choose material:</label>
-	<select id="material" name="material">
+                $query = "SELECT * FROM supplierreg WHERE material = '{$mat}'";
+                $result = $mysqli->query($query);
 
-      <option value="mat1">cotton</option>
-      <option value="mat2">silck</option>
-      <option value="mat3">wool</option>
+                if ($result->num_rows > 0) {
+                    echo "<table border='1'>
+                            <tr>
+                                <th>Registration Number</th>
+                                <th>Registered Name</th>
+                                <th>Address</th>
+                                <th>Contact Number</th>
+                                <th>Email</th>
+                                <th>Director Name</th>
+                                <th>NIC</th>
+                                <th>Director Contact Number</th>
+                                <th>Material</th>
+                            </tr>";
 
-	  <input type="submit" value="ok"  style="background-color: grey; color:white;">
-    </select>
-	</form><br></br>
-	<?php
-		include 'supDS.php';
-	?> <br></br>
-	<form style="background-color: white;">
-	<label for="supplier">Choose supplier:</label>
-	<select id="supplier" name="product">
-	<option value="" selected hidden></option>
-      <option value="supplier1">Option 1</option>
-      <option value="supplier2">Option 2</option>
-      <option value="supplier3">Option 3</option>
-    </select>
-	</form><br></br>
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['Registration_number'] . "</td>";
+                        echo "<td>" . $row['Registered_name'] . "</td>";
+                        echo "<td>" . $row['Address'] . "</td>";
+                        echo "<td>" . $row['Contact_number'] . "</td>";
+                        echo "<td>" . $row['Email'] . "</td>";
+                        echo "<td>" . $row['Director_name'] . "</td>";
+                        echo "<td>" . $row['NIC'] . "</td>";
+                        echo "<td>" . $row['Director_C_number'] . "</td>";
+                        echo "<td>" . $row['material'] . "</td>";
+                        echo "</tr>";
+                    }
 
+                    echo "</table>";
+                } else {
+                    echo "<p>No suppliers found for the selected material.</p>";
+                }
 
-	<form style="background-color: white;">
-	<label for="supplierregNumber">Supplier registeration number:</label>
-    <input type="text" id="supplierregNumber" name="supplierregNumber">
-	</form><br></br>
+                $mysqli->close();
+            }
+            ?><br></br>
 
+            <form style="background-color: white;">
+                <label for="supplier">Choose supplier:</label>
+                <select id="supplier" name="product">
+                    <option value="" selected hidden></option>
+                    <option value="supplier1">Option 1</option>
+                    <option value="supplier2">Option 2</option>
+                    <option value="supplier3">Option 3</option>
+                </select>
+            </form><br></br>
 
+            <form style="background-color: white;">
+                <label for="supplierregNumber">Supplier registration number:</label>
+                <input type="text" id="supplierregNumber" name="supplierregNumber">
+            </form><br></br>
 
+            <form style="background-color: white;">
+                <label for="companyID">Company ID:</label>
+                <input type="text" id="companyID" name="companyID">
+            </form><br></br>
 
-	<form style="background-color: white;">
-	<label for="companyID">Company ID:</label>
-    <input type="text" id="companyID" name="companyID" >
-    </form><br></br>
+            <form style="background-color: white; ">
+                <label for="quantity">Quantity:</label>
+                <input type="text" id="quantity" name="quantity">
+            </form><br></br>
 
-	<form style="background-color: white; ">
-    <label for="quantity">Quantity:</label>
-    <input type="text" id="quantity" name="quantity" >
-    </form><br></br>
+            <form style="background-color: white;">
+                <label for="delivery-date">Delivery Date:</label>
+                <input type="date" id="delivery-date" name="delivery-date">
+            </form><br></br>
 
-	<form style="background-color: white;">
-    <label for="delivery-date">Delivery Date:</label>
-    <input type="date" id="delivery-date" name="delivery-date">
-	</form><br></br>
+            <div class="subbutton">
+                <input type="submit" value="Proceed" style="background-color: white; color: black;" onclick="showAlert()">
+            </div>
 
-    <!--input type="checkbox" id="payment-proceed" name="payment-proceed">
-    <label for="payment-proceed">Proceed with Payment</label-->
-
-	<div class="subbutton">
-    <input type="submit" value="Proceed" style="background-color: white; color: black;" onclick="showAlert()">
-	</div>
-
-	<script>
-	function showAlert() {
-		alert("sucessful!");
-	}
-	</script>
-
-  </form>
-  </div>
-</div>
-<hr>
-
-<?php include 'footer.php';?>
+            <script>
+                function showAlert() {
+                    alert("Successful!");
+                }
+            </script>
+        </div>
+    </div>
+    <hr>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
